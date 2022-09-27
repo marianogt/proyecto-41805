cee8d90e036e5992bac5f44e387de68d9282a7d3
Mariano Torochta <marianogt@gmail.com>
2022-09-26 22:43:15 -0300

    agregado de vaciado de carrito y mas descripciones

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-let ingreso = prompt("Selecciona el artículo a Comprar: \n 1- Instrumento \n 2- Medidor \n 3- Barredora \n 4- Cámara \n 5- F para finalizar");
+let ingreso = prompt("Selecciona el artículo a Comprar: \n 1- Instrumento \n 2- Medidor \n 3- Barredora \n 4- Cámara \n V - Para Vaciar Carro \n F - para finalizar");
 let cantI=0, cantM= 0, cantB= 0, cantC = 0;
 let sumI=0, sumM=0, sumB=0, sumC=0; 
 
@@ -22,30 +22,38 @@ let res=0, suma=0;
 while ((ingreso != "F") && (ingreso != "f")) {
     switch (ingreso) {
         case "1":
-          cantI++;  
-          sumI= sumI + eleI.precio;
-          console.log("Selccionaste Instrumento/s por un valor de: " + sumI);
-          break;
+            let depositoI = parseInt(prompt("Ingresa la catidad a comprar"));
+            cantI = cantI + depositoI;
+            sumI=  eleI.precio * cantI;
+            console.log("Compraste " + cantI + " Instrumento/s por un valor de: " + sumI);
+            break;
         case "2":
-            cantM++;  
-            sumM= sumM + eleM.precio;
-            console.log("Selccionaste Medidor/es por un valor de: " + sumM);
+            let depositoM = parseInt(prompt("Ingresa la catidad a comprar"));
+            cantM = cantM + depositoM;
+            sumM=  eleM.precio * cantM;
+            console.log("Compraste " + cantM + " Medidor/es por un valor de: " + sumM);
             break;
         case "3":
-            cantB++;  
-            sumB= sumB + eleB.precio;
-            console.log("Selccionaste Barredora/s por un valor de: " + sumB);
+            let depositoB = parseInt(prompt("Ingresa la catidad a comprar"));
+            cantB = cantB + depositoB;
+            sumB =  eleB.precio * cantB;
+            console.log("Compraste " + cantB + " Barredora/s por un valor de: " + sumB);
             break;
         case "4":
-            cantC++;  
-            sumC= sumC + eleC.precio;
-            console.log("Selccionaste Cámara/s por un valor de: " + sumC);
+            let depositoC = parseInt(prompt("Ingresa la catidad a comprar"));
+            cantC = cantC + depositoC;
+            sumC =  eleC.precio * cantC;
+            console.log("Compraste " + cantC + " Cámara/s por un valor de: " + sumC);
             break;
-        default:
+        case "V","v":
+            sumI=sumB=sumC=sumM=0;
+            cantI=cantM=cantB=cantC=0;
+            break;
+          default:
           console.log("opcion no valida");
           break;
       }
-      ingreso = prompt("Selecciona el artículo a Comprar: \n 1- Instrumento \n 2- Medidor \n 3- Barredora \n 4- Cámara \n 5- F para finalizar");
+      ingreso = prompt("Selecciona el artículo a Comprar: \n 1- Instrumento \n 2- Medidor \n 3- Barredora \n 4- Cámara \n V- Para Vaciar Carro \n F- para finalizar");
     }
 res=sumar(cantI,cantM,cantB,cantC);
 suma=sumI+sumM+sumB+sumC;
@@ -56,13 +64,13 @@ if (res == 0){
 else
     {
     console.log("El Total de Productos Comprados es " + res + " por un Monto de USD: " + suma);
-    if(cantI > 0 ) console.log("Seleccionaste " + cantI+ " Instrumentos");
+    if(cantI > 0 ) console.log("Compraste " + cantI+ " Instrumentos por un Monto de USD: " + sumI);
      else console.log("No Seleccionaste Instrumentos");
-     if(cantM > 0 ) console.log("Seleccionaste " + cantM+ " Medidores");
+     if(cantM > 0 ) console.log("Compraste " + cantM+ " Medidores por un Monto de USD: " + sumM);
      else console.log("No Seleccionaste Medidores");
-     if(cantB > 0 ) console.log("Seleccionaste " + cantB+ " Barredoras");  
+     if(cantB > 0 ) console.log("Compraste " + cantB+ " Barredoras por un Monto de USD: " + sumB);  
      else console.log("No Seleccionaste Barredoras");
-     if(cantC > 0 ) console.log("Seleccionaste " + cantC+ " Cámaras");
+     if(cantC > 0 ) console.log("Compraste " + cantC+ " Cámaras por un Monto de USD: " + sumC);
      else console.log("No Seleccionaste Cámaras");
      alert("El Total de Productos Comprados es " + res + " por un Monto de USD: " + suma);
     }
