aa676016878d96e66ede4812803b29c017490036
marianogt <marianogt@gmail.com>
2022-11-08 14:03:02 -0300

    carga de tarjetas en el DOM

--- js/main.js
+++ js/main.js
@@ -1,274 +1,171 @@
 
 const date = new Date();
 
-let ingreso = prompt(date.toLocaleString()  + " Selecciona el artículo a Comprar: \n 1- Instrumento \n 2- Medidor \n 3- Barredora \n 4- Cámara \n V - Para Vaciar Carro \n F - Finaliza compra");
-let cantI=0, cantM= 0, cantB= 0, cantC = 0;
-let sumI=0, sumM=0, sumB=0, sumC=0; 
+document.getElementById("hor").innerHTML = " Selecciones los productos a comprar - " + date.toLocaleString();
 
- function producto(elemento, precio){
-   this.elemento = elemento;
-   this.precio = precio; 
- }
+const contenedor = document.querySelector(".contenedor"),
+      btn = document.querySelector("#btn"),
+      contenedor2= document.querySelector(".contenedor2"),
+      stk = document.querySelector("#stk")
+      ;
 
- const productosarray = [
-    { id: 1, nombre: "Instrumental", precio: 5, stock: 20  },
-    { id: 2, nombre: "Medidor", precio: 8, stock: 20 },
-    { id: 3, nombre: "Barredora", precio: 35, stock: 10 },
-    { id: 4, nombre: "Camara", precio: 25, stock: 10 },
-    { id: 5, nombre: "Alarma", precio: 35, stock: 10 },
-    { id: 6, nombre: "Sensor", precio: 12, stock: 10 },
-    { id: 7, nombre: "Sirena", precio: 20, stock: 10 },
-  ];
+let productosarray = [];
+let carritoc = [];
 
- const carrito = [];
-
- const eleI = new producto(1, 5);
- const eleM = new producto(2, 8);
- const eleB = new producto(3, 35);
- const eleC = new producto(4, 25);
-
-let res=0, suma=0;
- function sumar(I, M, B, C){
-   let res = 0;
-   return(res = parseInt(I) + parseInt(M) + parseInt(B) + parseInt(C));
- }
- 
- function buscarElemento(arr, filtro) {
-     const encontrada = arr.find((f) => {
-         return f.elemento === filtro;
-     })
-     return encontrada;
- }
- 
- function restarStock(arr, filtro, cantidad) {
-     let encontrada = arr.find((f) => {
-         return f.elemento === filtro;
-     })
-     if (encontrada) encontrada.stock -= cantidad;
- }
-
- function sumarStock(arr, filtro, cantidad) {
-    let encontrada = arr.find((f) => {
-        return f.elemento === filtro;
-    })
-    if (encontrada) encontrada.stock += cantidad;
-}
-
-function sumarCompra(arr, filtro, cantidad) {
-    let encontrada = arr.find((f) => {
-        return f.elemento === filtro;
-    })
-    if (encontrada) encontrada.cantidad += cantidad;
-}
-
-/* if (localStorage.getItem('carrito')){
-    carrito=JSON.parse(localStorage.getItem(carrito));
- } else {
-    carrito = [];
- }
- */ 
-//constructor de elementos para el carrito
- class elecarrito{
-    constructor(elemento, precio, cantidad){
-        this.elemento = elemento;
-        this.precio = precio;
-        this.cantidad = cantidad;
-    }
-}
-
-function buscarencarrito(elecarrito){
-    let elefound = carrito.find((carrito)=>{
-        return carrito.elemento === elecarrito.elemento;
-    })
-if(elefound){
-    elefound.cantidad = elefound.cantidad + elecarrito.cantidad;
-    document.getElementById('ins').innerText="Elemento encontrado " + elefound.elemento + " se compro " + elefound.cantidad;
-    console.log(elefound.cantidad);
-}
-else{
-//   document.querySelector('ins').innerText="Elemento NO encontrado";
-  document.getElementById('ins').innerText="Elemento NO encontrado ";
-  return carrito.push(elecarrito);
-}
-}
-
-function guardarenCarrito(elecarrito){
-    return carrito.push(elecarrito);
-}
-
-document.getElementById("bor").onclick = function() {//borrado por boton y no automático para que se pueda ver el localstorage 
-	localStorage.clear();    
-}
-
-/* function cargarcarro(){
-    carrito.elemento = l.getItem("key");
-}
-
-localStorage.getItem(array.forEach(element => {
-    carrito.push(element.elemento    
-});
-);
- */
-while ((ingreso != "F") && (ingreso != "f")) {
-    switch (ingreso) {
-            case "1":
-                let depositoI = parseInt(prompt("Ingresa la catidad a comprar"));
-                if ((cantI + depositoI) > productosarray[0].stock ){//Si la compra supera el stock aviso
-                    alert("Cantidad requerida supera el stock");
-                    swal({
-                        title: "Cantidad requerida supera el stock",
-                        text: "Presione el boton!",
-                        icon: "warning",
-                        button: "OK SALIR!",
-                      })//,{
-                    //     timer: 3000
-                    // });                    
-                    break;
-                }  
-                cantI = cantI + depositoI;
-                sumI=  eleI.precio * cantI;
-                console.log("Compraste " + cantI + " Instrumento/s por un valor de: " + sumI);
-                let newelemento1 = new elecarrito("Instrumento",eleI.precio,cantI);
-                if(buscarElemento(carrito, newelemento1.elemento)) // si el elemento ya se compró agrego cantidad
-                {
-                   sumarCompra(carrito, 'Instrumento', depositoI);
-                }
-                else{
-                    carrito.push(newelemento1); // primera compra
-                }
-//                restarStock(carrito, 'Instrumento', depositoI);
-                localStorage.setItem("Instrumento", JSON.stringify(newelemento1));  // guardo en el local storage          
-                break;
-            case "2":
-                let depositoM = parseInt(prompt("Ingresa la catidad a comprar"));
-                if ((cantM + depositoM) > productosarray[1].stock ){//Si la compra supera el stock aviso
-                    alert("Cantidad requerida supera el stock");
-                    break;
-                }  
-                cantM = cantM + depositoM;
-                sumM=  eleM.precio * cantM;
-                console.log("Compraste " + cantM + " Medidor/es por un valor de: " + sumM);
-                let newelemento2 = new elecarrito("Medidor",eleM.precio,cantM);
-                if(buscarElemento(carrito, newelemento2.elemento)) // si el elemento ya se compró agrego cantidad
-                {
-                    sumarCompra(carrito, 'Medidor', depositoM);
-                }
-                else{
-                    carrito.push(newelemento2); // primera compra
-                }
-        //                restarStock(carrito, 'Medidor', depositoM);
-                localStorage.setItem("Medidor", JSON.stringify(newelemento2));  // guardo en el local storage          
-                break;
-        case "3":
-                let depositoB = parseInt(prompt("Ingresa la catidad a comprar"));
-                if ((cantB + depositoB) > productosarray[2].stock ){//Si la compra supera el stock aviso
-                    alert("Cantidad requerida supera el stock");
-                    break;
-                }  
-                cantB = cantB + depositoB;
-                sumB =  eleB.precio * cantB;
-                console.log("Compraste " + cantB + " Barredora/s por un valor de: " + sumB);
-                let newelemento3 = new elecarrito("Barredora",eleB.precio,cantB);
-                if(buscarElemento(carrito, newelemento3.elemento)) // si el elemento ya se compró agrego cantidad
-                {
-                    sumarCompra(carrito, 'Barredora', depositoB);
-                }
-                else{
-                    carrito.push(newelemento3); // primera compra
-                }
-        //                restarStock(carrito, 'Barredora', depositoB);
-                localStorage.setItem("Barredora", JSON.stringify(newelemento3));  // guardo en el local storage          
-                break;
-        case "4":
-                let depositoC = parseInt(prompt("Ingresa la catidad a comprar"));
-                if ((cantC + depositoC) > productosarray[3].stock ){//Si la compra supera el stock aviso
-                    alert("Cantidad requerida supera el stock");
-                    break;
-                }  
-                cantC = cantC + depositoC;
-                sumC =  eleC.precio * cantC;
-                console.log("Compraste " + cantC + " Cámara/s por un valor de: " + sumC);
-                let newelemento4 = new elecarrito("Camara",eleC.precio,cantC);
-                if(buscarElemento(carrito, newelemento4.elemento)) // si el elemento ya se compró agrego cantidad
-                {
-                sumarCompra(carrito, 'Camara', depositoC);
-                }
-                else{
-                    carrito.push(newelemento4); // primera compra
-                }
-    //                restarStock(carrito, 'Camara', depositoI);
-                localStorage.setItem("Camara", JSON.stringify(newelemento4));  // guardo en el local storage          
-                break;
-        case "V","v":
-            sumI=sumB=sumC=sumM=0;
-            cantI=cantM=cantB=cantC=0;
-            while(carrito.length > 0){ 
-                carrito.pop()}; 
-            break;
-          default:
-          console.log("opcion no valida");
-          break;
-      }
-      ingreso = prompt("Selecciona el artículo a Comprar: \n 1- Instrumento \n 2- Medidor \n 3- Barredora \n 4- Cámara \n V- Para Vaciar Carro \n F- Finaliza compra");
-    }
-res=sumar(cantI,cantM,cantB,cantC);
-suma=sumI+sumM+sumB+sumC;
-if (res == 0){
- console.log("No compraste ningún producto")   
- alert("No compraste ningún producto");
- }
-else
-    {
-    console.log("El Total de Productos Comprados es " + res + " por un Monto de USD: " + suma);
-    if(cantI > 0 ){ 
-  //      carrito.push(["Instrumentos", cantI, sumI]); 
-        console.log("Compraste " + cantI+ " Instrumentos por un Monto de USD: " + sumI);
-    }
-     else console.log("No Seleccionaste Instrumentos");
-    if(cantM > 0 ){ 
-        console.log("Compraste " + cantM+ " Medidores por un Monto de USD: " + sumM);
-    } 
-     else console.log("No Seleccionaste Medidores");
-    if(cantB > 0 ){ 
-        console.log("Compraste " + cantB+ " Barredoras por un Monto de USD: " + sumB);  
-    }
-     else console.log("No Seleccionaste Barredoras");
-    if(cantC > 0 ){
-        console.log("Compraste " + cantC+ " Cámaras por un Monto de USD: " + sumC);
-    }
-     else console.log("No Seleccionaste Cámaras");
-     alert("El Total de Productos Comprados es " + res + " por un Monto de USD: " + suma);
-     console.log(carrito);
-    }
-let formapago=0, cuotas=0;
-if(suma>0) {
-    while (formapago==0 && formapago !=1 && formapago !=2 && formapago !=3){
-        formapago = prompt("Como quiere abonar? \n 1- Efectivo \n 2- Tarjeta de Debito  \n 3- Tarjeta de Credito");
-    }
-    if (formapago== 3){
-        while (cuotas==0 && cuotas !=1 && cuotas !=2 && cuotas !=3){
-        cuotas= prompt("Encuantas cuotas? \n 1- 3 Cuotas \n 2- 6 Cuotas  \n 3- 12 Cuotas");
+function getcar(){
+    let cont = 0;
+    for (let i = 1; i < 9; i++) {
+        let getJson = localStorage.getItem("id" + (i))
+        if(getJson){
+            carritoc[cont] = JSON.parse(getJson);
+            cont +=1;
         }
-        document.getElementById('con').innerText= "Su compra fue exitosa, en " + cuotas + " cuotas por un monto total de USD " + suma;
-        // console.log("Su compra fue exitosa, en " + cuotas + " cuotas por un monto total de USD " + suma);
-        alert("Su compra fue exitosa, en " + cuotas + " cuotas por un monto total de USD " + suma);
-    }
-    else 
-        {
-         if(formapago==1) {
-            document.getElementById('con').innerText= "Su compra fue exitosa, en efectivo por un monto de USD " + suma;
-            // console.log("Su compra fue exitosa, en efectivo por un monto de USD " + suma);
-            alert("Su compra fue exitosa, en efectivo por un monto de USD " + suma);
-         }
-         else{
-            document.getElementById('con').innerText= "Su compra fue exitosa, con tarjeta de debito un monto de USD " + suma;
-            // console.log("1Su compra fue exitosa, con tarjeta de debito un monto de USD " + suma);
-            alert("Su compra fue exitosa, con tarjeta de debito un monto de USD " + suma);
-         }
         }
-    // localStorage.clear();    
-}   
+    for (let i = 0; i < carritoc.length; i++) {
+        let encontrada = productosarray.find(f => f.id === carritoc[i].id);
+        if (encontrada)  encontrada.stock = encontrada.stock - carritoc[i].stock;
+        }    
+}
 
-// for (const propiedad in ele){
-//   console.log(propiedad + ":" + ele[propiedad]);
-// }
+const dibujartarjetas = (arr) => {
+    // función que genere la vista de los productos
+    getcar();
+    let html;
+    for (const item of arr) {
+      const { id, nombre, img, precio, descripcion, stock } = item;
+       html = `
+       <div class="card">
+        <img class="card__image" src="../imagenes/productos/${img}" alt=${nombre.toUpperCase()}>
+        <h3 class="card__title">${id} - ${nombre.toUpperCase()}</h3>
+        <div class="card-content">
+        
+        </div>
+        <p class="card__description">${descripcion}</p>
+        <h3 id="con${id-1}"> Stock del Item ${id}: ${stock} </h3> 
+        <button class="card__boton" id="${btn}" onclick="calcularcompra(${id})" >Comprar</button>
+        <button class="card__boton" onclick="calculardevolucion(${id})" >Devolver</button>
+       </div>`;
+      contenedor.innerHTML += html;
+    }
+    Swal.fire;
+    pintarcarrrito(carritoc);
+  };
+
+function pintarcarrrito(arr){
+    let html = '';
+    contenedor2.innerHTML = "";
+    arr.forEach((articulo,index) => {
+        html = `
+        <div class="contenedor3">
+        <span class="anchon">Articulo: ${articulo.nombre}</span>
+        <span class="anchop">Precio: $${articulo.precio} c/u</span>
+        <span class="anchos">Cant.: ${articulo.stock}</span>
+        <button id=${index} class="botb">X</button>
+        </div> `;
+       contenedor2.innerHTML += html;
+    });
+    let botones = document.querySelectorAll(".botb");
+    botones.forEach(boton => {
+        boton.addEventListener("click",quitar);
+    }) 
+}
+
+function quitar(e){
+    const id = e.srcElement.id;
+    if(localStorage.getItem("id" +carritoc[id].id)){
+        localStorage.removeItem("id" + carritoc[id].id);
+    document.getElementById("con" + (carritoc[id].id-1)).innerHTML = "Stock del Item " + carritoc[id].id +" : " + (parseInt(productosarray[carritoc[id].id-1].stock) + parseInt(carritoc[id].stock));
+    carritoc.splice(id, 1);
+    pintarcarrrito(carritoc);
+    }
+}
+
+function calcularcompra(numero){
+    let stockl=0;
+    if (productosarray[numero-1].stock > 0 ){
+        productosarray[numero-1].stock -=1;
+        if ((carritoc.length) && (existe(carritoc, productosarray[numero-1].id) != -1)) {
+            stockl = existe(carritoc, productosarray[numero-1].id).stock += 1;        
+        }
+        else {
+            carritoc.push({id: productosarray[numero-1].id, nombre: productosarray[numero-1].nombre, stock: 1, precio: productosarray[numero-1].precio});
+            localStorage.setItem("id" + productosarray[numero-1].id,  JSON.stringify({id: productosarray[numero-1].id, nombre: productosarray[numero-1].nombre, stock: 1, precio: productosarray[numero-1].precio}));  // guardo en el local storage          
+            stockl = 1;
+        }
+        document.getElementById("con" + (numero-1)).innerHTML = "Stock del Item " + numero +" : " + productosarray[numero-1].stock;
+        }
+    else {
+        stockl = existe(carritoc, productosarray[numero-1].id).stock;
+        Swal.fire({
+            position: 'center',
+            icon: 'warning',
+            title: 'La cantidad a comprar supera el stock',
+            showConfirmButton: false,
+            timer: 2000
+          })     
+    }    
+    if(localStorage.getItem("id" + productosarray[numero-1].id)){
+        localStorage.removeItem("id" + productosarray[numero-1].id);
+        localStorage.setItem("id" + productosarray[numero-1].id , JSON.stringify({id: productosarray[numero-1].id, nombre: productosarray[numero-1].nombre, stock: stockl, precio: productosarray[numero-1].precio}));  // guardo en el local storage          
+    }
+    pintarcarrrito(carritoc);
+    document.getElementById("tot").innerHTML = "";
+}
+  
+function existe(arraycar, idelemento){
+    return arraycar.find(ele => ele.id === idelemento) ?? -1;
+}
+
+  function calculardevolucion(numero){
+    let stockl = 0;
+    if((carritoc.length) && (existe(carritoc, productosarray[numero-1].id) != -1) && (existe(carritoc, productosarray[numero-1].id).stock > 0)) {
+        productosarray[numero-1].stock +=1;
+        stockl = existe(carritoc, productosarray[numero-1].id).stock -=1;
+        if (!stockl) {
+           let aux = carritoc.findIndex(id => id.id === productosarray[numero-1].id);
+            if(aux != -1) { carritoc.splice(aux,1); }
+            if(localStorage.getItem("id" + productosarray[numero-1].id)){
+                localStorage.removeItem("id" + productosarray[numero-1].id);}
+            document.getElementById("con" + (numero-1)).innerHTML = "Stock del Item " + numero +" : " + productosarray[numero-1].stock;            
+        }
+        else{
+            document.getElementById("con" + (numero-1)).innerHTML = "Stock del Item " + numero +" : " + productosarray[numero-1].stock;
+            if(localStorage.getItem("id" + productosarray[numero-1].id)){
+               localStorage.removeItem("id" + productosarray[numero-1].id);
+               localStorage.setItem("id" + productosarray[numero-1].id , JSON.stringify({id: productosarray[numero-1].id, nombre: productosarray[numero-1].nombre, stock: stockl, precio: productosarray[numero-1].precio}));  // guardo en el local storage          
+            }
+
+        }
+    }
+    pintarcarrrito(carritoc);
+  }
+
+document.getElementById("resu").onclick = function() {//borrado por boton y no automático para que se pueda ver el localstorage 
+    let cant=0, monto=0;
+    for (let i = 0; i < carritoc.length; i++) {
+        cant = carritoc[i].stock + cant;
+        monto = carritoc[i].precio * carritoc[i].stock + monto;
+    }
+    document.getElementById("tot").innerHTML = " Cantidad de elementos comprados: " + cant + " por un monto USD: "+ monto;
+    localStorage.clear();    
+    carritoc=[];
+    pintarcarrrito(carritoc);
+}
+
+ const URL = "./datos/datos.json";
+ const cargatarjetas = async (URL) => {
+   const respuesta = await fetch(URL);
+   productosarray = await respuesta.json();
+  dibujartarjetas(productosarray);
+ };
+
+cargatarjetas(URL);
+
+Swal.fire({
+    position: 'top',
+    icon: 'success',
+    title: 'Bienvenidos a la pagina de compras de Dylon',
+    showConfirmButton: false,
+    timer: 2500
+  })
+ 
